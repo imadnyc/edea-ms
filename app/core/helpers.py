210f80f78179e9415dccab702b72eefa607e8408
Elen Eisendle <ln@calcifer.ee>
2023-07-07 22:01:41 +0200

    add type annotations to core helpers

--- app/core/helpers.py
+++ app/core/helpers.py
@@ -1,4 +1,5 @@
 from app.db import models
+from sqlalchemy.orm import InstrumentedAttribute
 
 
 async def tryint(ident: str) -> int | str:
@@ -8,9 +9,13 @@ async def tryint(ident: str) -> int | str:
         return ident
 
 
-def tr_unique_field(id):
+def tr_unique_field(
+    id: int | str,
+) -> InstrumentedAttribute[int] | InstrumentedAttribute[str]:
     return models.TestRun.short_code if isinstance(id, str) else models.TestRun.id
 
 
-def prj_unique_field(id: int | str):
+def prj_unique_field(
+    id: int | str,
+) -> InstrumentedAttribute[int] | InstrumentedAttribute[str]:
     return models.Project.number if isinstance(id, str) else models.Project.id
